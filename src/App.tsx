5a76040e3f3ea796a146890fcb9c1e58e8442c19
79209 <IlyaBogom@gmail.com>
2022-06-24 10:30:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react';
+import './styles/App.scss';
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+const App = () => {
+    return (
+        <div className="app">
+
+        </div>
+    );
+};
+
+export default App;
